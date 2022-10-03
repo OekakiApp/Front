1b8c438f6734ac4efef1fab901f41654915c538a
Hiro <hiro@HironoiMac.local>
2022-10-04 00:35:00 +0900

    chore: pugの導入と、テストフレームワークの導入

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,29 +6,20 @@ defineProps<{ msg: string }>()
 const count = ref(0)
 </script>
 
-<template>
-  <h1>{{ msg }}</h1>
+<template lang="pug">
+h1 {{ msg }}
+.card
+  button(type="button" @click="count++") count is {{ count }}
+  p Edit
+    code components/HelloWorld.vue
+    | to test HMR
 
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+p Check out
+  a(href="https://vuejs.org/guide/quick-start.html#local" target="_blank") create-vue,
+  | the official Vue + Vite starter Install
+  a(href="https://github.com/johnsoncodehk/volar" target="_blank")
+  | Volar in your IDE for a better DX
+.read-the-docs Click on the Vite and Vue logos to learn more
 </template>
 
 <style scoped>
