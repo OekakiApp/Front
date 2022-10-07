33e61f5ec251fc67d5ebeff91bfeddb30aee3f55
harutas <96802323+harutas@users.noreply.github.com>
2022-10-07 23:11:42 +0900

    feat: added drawing and text

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import './style.css'
+import VueKonva from 'vue-konva'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const pinia = createPinia()
+const app = createApp(App)
+
+app.use(pinia).use(VueKonva).mount('#app')
---
+++ src/stores/konva/line.ts
@@ -0,0 +1,66 @@
+import { defineStore } from 'pinia'
+
+interface Points {
+  points: number[]
+  color: string
+  strokeWidth: number
+}
+
+const useStoreLine = defineStore({
+  id: 'line',
+  state: () => ({
+    isDrawing: false,
+    drawColor: 'black', // default Color
+    strokeWidth: 1, // default stroke width
+    lines: [] as Points[],
+  }),
+
+  actions: {
+    setColor(selectedColor: string) {
+      this.drawColor = selectedColor
+    },
+
+    setStrokeWidth(selectedStrokeWidth: string) {
+      this.strokeWidth = parseInt(selectedStrokeWidth, 10)
+    },
+
+    setLines(line: Points) {
+      this.lines = [...this.lines, line]
+    },
+
+    handleMouseDown(e: any, selectedMode: string) {
+      if (selectedMode !== 'draw') return
+      if (e.target !== e.target.getStage()) return
+      this.isDrawing = true
+      const pos = e.target.getStage().getPointerPosition()
+      const points = {
+        points: [pos.x, pos.y],
+        color: this.drawColor,
+        strokeWidth: this.strokeWidth,
+      }
+      this.setLines(points)
+    },
+
+    handleMouseMove(e: any) {
+      // no drawing - skipping
+      if (!this.isDrawing) {
+        return
+      }
+      // ステージを取得
+      const stage = e.target.getStage()
+      // ステージのx,yを取得
+      const point = stage.getPointerPosition()
+      const lastLine = this.lines[this.lines.length - 1]
+      // add point
+      lastLine.points = lastLine.points.concat([point.x, point.y])
+      // // replace last
+      this.lines.splice(this.lines.length - 1, 1, lastLine)
+    },
+
+    handleMouseUp() {
+      this.isDrawing = false
+    },
+  },
+})
+
+export default useStoreLine
