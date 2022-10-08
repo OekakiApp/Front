a78ce64b65dd22a0609cc78bc4104f8879b57cb2
Hiro <hiro@HironoiMac.local>
2022-10-08 19:40:51 +0900

    feat: headerコンポーネントを作成

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,15 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomePage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'Home',
-      component: HomeView,
-    },
-  ],
+  routes: [],
 })
 
 export default router
