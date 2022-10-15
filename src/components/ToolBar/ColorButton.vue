05c856dafd586cd4b08c7a49ad6b8d6d5e7282a3
harutas <96802323+harutas@users.noreply.github.com>
2022-10-15 10:09:46 +0900

    feat: add color-picker and marker

--- src/components/ToolBar/ColorButton.vue
+++ src/components/ToolBar/ColorButton.vue
@@ -1,16 +1,33 @@
 <script setup lang="ts">
 import { type Color } from '../ToolBar.vue'
-import useStoreLine from '../../stores/konva/line'
 
-type Props = {
+interface Props {
   color: Color
+  index: number
+  activeindex: number | null
+}
+
+interface Emits {
+  (e: 'toggle-active', value: number): void
 }
 
 const props = defineProps<Props>()
-const { setColor } = useStoreLine()
+const emit = defineEmits<Emits>()
+
+const handleClick = () => {
+  emit('toggle-active', props.index)
+}
 </script>
 
 <template lang="pug">
-button(class="items-center" @click="setColor(color)")
-  div(class="rounded-full w-8 h-8 m-1 hover:scale-125" :style="{'background-color': props.color}")
+div(class="rounded-full w-10 h-10 flex justify-center items-center mx-1" :class="{'active-circle': props.index === props.activeindex }")
+  button(class="items-center" @click="() => {props.color.onClick();handleClick()}")
+    div(class="rounded-full w-8 h-8 m-0 cursor-pointer" :style="props.color.style")
 </template>
+
+<style scoped>
+.active-circle {
+  border-width: 2px;
+  border-color: rgb(147 51 234);
+}
+</style>
---
+++ src/components/ToolBar/ColorPickerPalette.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { storeToRefs } from 'pinia'
+import { ColorPicker } from 'vue-color-kit'
+import 'vue-color-kit/dist/vue-color-kit.css'
+import useStoreColor, { type Color } from '../../stores/color'
+import useStoreLine from '../../stores/konva/line'
+
+const { color, colorsDefault } = storeToRefs(useStoreColor())
+const { changeColor } = useStoreColor()
+const { setColor } = useStoreLine()
+
+const onchange = (colorObj: Color) => {
+  changeColor(colorObj)
+  // 線の色を変更
+  const { r, g, b, a } = colorObj.rgba
+  setColor(`rgba(${r}, ${g}, ${b}, ${a})`)
+}
+</script>
+
+<template lang="pug">
+
+ColorPicker(
+  theme="light"
+  :color="color"
+  :colors-default="colorsDefault"
+  :sucker-hide="true"
+  @change-color="onchange")
+
+</template>
+
+<style scoped>
+.hu-color-picker {
+  width: 220px !important;
+}
+</style>
