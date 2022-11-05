173084de732477a3bcefde620173ff41c2cd8d5f
harutas <96802323+harutas@users.noreply.github.com>
2022-11-06 02:00:49 +0900

    fix: bug fontsize and cursor

--- src/components/ToolBar/FontSizeSelect.vue
+++ src/components/ToolBar/FontSizeSelect.vue
@@ -23,5 +23,5 @@ const checkFontSize = (value: number): string => {
 <template lang="pug">
 div(class="tooltip block mr-2" :data-tip="'Font size'")
   label(for="font-size" class="hidden mb-2 text-sm font-medium text-gray-900 dark:text-gray-300") Font size
-  input(id="font-size" ref="selectedFontSize" :value="fontSize" type="number" class="block p-1 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 text-sm focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required max="100" min="1" @input="() => {setFontSize(checkFontSize(selectedFontSize));setTextOptionValue('fontSize', checkFontSize(selectedFontSize));}")
+  input(id="font-size" ref="selectedFontSize" :value="fontSize" type="number" class="block p-1 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 text-sm focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required max="100" min="1" @input="() => {setFontSize(checkFontSize(selectedFontSize.value));setTextOptionValue('fontSize', checkFontSize(selectedFontSize.value));}")
 </template>
