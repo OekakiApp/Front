05565d83768a05321ee1ce6881bfdb7fd33b9cc7
Hiro <hiro@HironoiMac.local>
2022-11-06 22:36:09 +0900

    fix: toppageのpaddingを調整

--- src/views/TopPage.vue
+++ src/views/TopPage.vue
@@ -25,14 +25,14 @@ const faqs: faqType[] = [
 div
   //- First View
   div(class="text-center")
-    div(class="pt-96 pb-24")
+    div(class="pt-72 pb-24")
       h1(class="text-9xl title mb-8") Graimer
       p Introduce Yourself with full of imagination
     p(class="mt-6 text-2xl") 自由な発想で、世界に一人の自分を紹介しよう
-    div(class="d-flex mt-12 mb-48")
+    div(class="d-flex mt-12 mb-36")
       router-link(class="btn btn-primary ml-6" to="sign_up") 新規登録する
   //- How To Use
-  div(class="card w-full p-24 bg-base-100 shadow-xl bg-slate-100 mb-36")
+  div(class="card w-full p-16 bg-base-100 shadow-xl bg-slate-100 mb-36")
     div(class="card-body")
       h2(class="card-title text-5xl") How to Use
       p(class="text-2xl mb-6") 〜Graimerの使い方〜
@@ -44,7 +44,7 @@ div
           li(class="step") 公開する
 
   //- FAQ
-  div(class="card w-full p-24 bg-base-100 shadow-xl bg-slate-100 mb-36")
+  div(class="card w-full p-16 bg-base-100 shadow-xl bg-slate-100 mb-36")
     div(class="card-body")
       h2(class="card-title text-5xl") FAQ
       p(class="text-2xl") 〜よくある質問〜
