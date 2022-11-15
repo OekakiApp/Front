5f16d2cefe8748d4be87ebdb623766e6be2c3f34
haruki111 <yuji515091@gmail.com>
2022-11-15 23:49:26 +0900

    feat: lp_モバイル対応

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -25,11 +25,11 @@ nav(class="bg-bleachWhite px-2 sm:px-4 py-2.5 rounded")
       div(v-if="isLoggedIn")
         router-link(to="/users")
           img(:src="icon" class="mini-avatar ring-2 ring-gray-700 ")
-      ul(v-else class="flex flex-col p-2 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0")
+      ul(v-else class="flex text-sm p-2 space-x-2 rounded-lg sm:space-x-8 sm:text-lg sm:font-medium")
         li
-          router-link(to="/sign_up" class="text-lg text-midnightBlue hover:text-dustyOrange" :class='{"text-dustyOrange": isSignUpPage}') 新規登録
+          router-link(to="/sign_up" class=" text-midnightBlue hover:text-dustyOrange" :class='{"text-dustyOrange": isSignUpPage}') 新規登録
         li
-          router-link(to="/login" class="text-lg hover:text-dustyOrange" :class='{"text-dustyOrange": isLoginPage}') ログイン
+          router-link(to="/login" class="text-midnightBlue hover:text-dustyOrange" :class='{"text-dustyOrange": isLoginPage}') ログイン
 </template>
 
 <style scoped>
@@ -38,5 +38,12 @@ nav(class="bg-bleachWhite px-2 sm:px-4 py-2.5 rounded")
   width: 55px;
   height: 55px;
   border-radius: 50%;
+}
+
+@media screen and (max-width: 639px) {
+  .mini-avatar {
+    width: 45px;
+    height: 45px;
+  }
 }
 </style>
