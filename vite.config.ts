2c9639524a98f8ab5a298a5b23a891d6815765ca
haruki honda <68400191+haruki111@users.noreply.github.com>
2022-11-20 15:08:42 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/Front/',
+  base: '/',
   build: {
     outDir: resolve(__dirname, 'docs'),
     assetsDir: './',
