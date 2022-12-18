edbc6ddba3b9009114016db66c81e5f04bf2841c
haruki111 <yuji515091@gmail.com>
2022-12-18 20:03:58 +0900

    chore: format

--- src/views/GalleryPage.vue
+++ src/views/GalleryPage.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { ref, reactive } from 'vue'
+
 const modalWidth =
   window.innerWidth <= 400
     ? ref(window.innerWidth * 0.9)
@@ -7,23 +8,23 @@ const modalWidth =
 const modalHeight = ref((modalWidth.value * 9) / 16)
 
 const modalStyle = reactive({
-  width: modalWidth.value + 'px',
-  height: modalHeight.value + 'px',
+  width: `${modalWidth.value}px`,
+  height: `${modalHeight.value}px`,
 })
 
 const resizeModal = () => {
   modalWidth.value =
     window.innerWidth <= 400 ? window.innerWidth * 0.9 : window.innerWidth * 0.7
   modalHeight.value = (modalWidth.value * 9) / 16
-  modalStyle.width = modalWidth.value + 'px'
-  modalStyle.height = modalHeight.value + 'px'
+  modalStyle.width = `${modalWidth.value}px`
+  modalStyle.height = `${modalHeight.value}px`
 }
 </script>
 
 <template lang="pug">
 div(class="my-8 grid gap-4 xl:grid-cols-3 md:grid-cols-2")
   div(v-for="n of 9" :key="n" class="picture m-auto")
-    label(@click="resizeModal()" for="modal" class="flex items-center")
+    label(for="modal" class="flex items-center" @click="resizeModal()")
       div(class="bg-gray-200 rounded-lg border border-gray-500 relative" style="width: 320px; height: 180px")
         div(class="heart absolute cursor-pointer" style="right: 10px; bottom: 10px")
       
@@ -33,7 +34,7 @@ div(class="my-8 grid gap-4 xl:grid-cols-3 md:grid-cols-2")
         div(class="tiny-avatar ring-2 bg-gray-200 ring-gray-700 ")
         div(class="text-midnightBlue text-sm pl-2") ネーム
 
-input(type="checkbox" id="modal" class="modal-toggle")
+input(id="modal" type="checkbox" class="modal-toggle")
 label(for="modal" class="modal cursor-pointer")
   div(class="bg-gray-200 rounded-lg border border-gray-500 relative" :style="modalStyle")
 
