d86b3aa7aa48d27f2a2eeeac44d721396c5b9224
harutas <96802323+harutas@users.noreply.github.com>
2023-04-02 23:28:48 +0900

    fix: history mode param

--- src/router/index.ts
+++ src/router/index.ts
@@ -19,7 +19,7 @@ import useStoreUserImage from '@/stores/userImage'
 /* eslint-enable */
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     {
       path: '/',
