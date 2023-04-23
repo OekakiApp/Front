83c17c77b4a7871eaad557698399bb7ed4d4be75
haruki111 <yuji515091@gmail.com>
2023-04-23 10:39:40 +0900

    feat: get share Canvases

--- src/views/GalleryPage.vue
+++ src/views/GalleryPage.vue
@@ -1,5 +1,51 @@
 <script setup lang="ts">
-import { ref, reactive } from 'vue'
+import { ref, reactive, onMounted, type Ref } from 'vue'
+import useAuthStore from '@/stores/auth'
+import { db } from '@/firebase/index'
+import {
+  query,
+  where,
+  collection,
+  getDocs,
+  getDoc,
+  doc,
+  Timestamp,
+  setDoc,
+} from 'firebase/firestore'
+import Icon from '@/assets/user_icon.png'
+
+interface shareCanvases {
+  title: string
+  id: string
+  uid: string
+  image: string
+  updatedAt: Timestamp
+  name: string
+  avator: string
+  isLike: boolean
+}
+
+interface galleryUser {
+  name: string
+  icon: string
+}
+
+interface heart {
+  isLike: boolean
+  addedAt: Timestamp
+}
+
+const authStore = useAuthStore()
+
+const galleries = ref(new Map<string, shareCanvases>())
+
+const likeHash: Ref<Record<string, heart>> = ref({})
+
+const testImage = ref('')
+
+onMounted(() => {
+  setShareCanvases()
+})
 
 const modalWidth =
   window.innerWidth <= 400
@@ -12,60 +58,109 @@ const modalStyle = reactive({
   height: `${modalHeight.value}px`,
 })
 
-const resizeModal = () => {
+const resizeModal = (image: string) => {
   modalWidth.value =
     window.innerWidth <= 400 ? window.innerWidth * 0.9 : window.innerWidth * 0.7
   modalHeight.value = (modalWidth.value * 9) / 16
   modalStyle.width = `${modalWidth.value}px`
   modalStyle.height = `${modalHeight.value}px`
+  testImage.value = image
 }
 
-const galleries = reactive([
-  {
-    title: 'エアリアル',
-    name: 'スレッタ',
-    avatar: 'https://g-witch.net/gwitch/jp/character/2022/10/01_front_s_2.png',
-    isLike: false,
-  },
-  {
-    title: 'ディランザ',
-    name: 'グエル',
-    avatar: 'https://g-witch.net/gwitch/jp/character/2022/10/03_front_s.png',
-    isLike: false,
-  },
-  {
-    title: 'ファラクト',
-    name: 'エラン',
-    avatar: 'https://g-witch.net/gwitch/jp/character/2022/10/04_front_s_2.png',
-    isLike: false,
-  },
-  {
-    title: 'ミカエリス',
-    name: 'シャディク',
-    avatar: 'https://g-witch.net/gwitch/jp/character/2022/10/05_front_s.png',
-    isLike: false,
-  },
-])
+const setShareCanvases = async () => {
+  await getDoc(doc(db, 'hearts', authStore.uid))
+    .then((heartDocSnap) => {
+      if (heartDocSnap.exists()) {
+        likeHash.value = heartDocSnap.data()
+      }
+    })
+    .catch((error) => {
+      console.log(error)
+    })
 
-const likeColor = (isLike: boolean) => (isLike ? 'fill: red' : 'fill: #fff')
+  const canvasQuery = query(
+    collection(db, 'canvas'),
+    where('uid', '!=', authStore.uid),
+    where('isShare', '==', true),
+  )
+  await getDocs(canvasQuery)
+    .then((querySnapshot) => {
+      querySnapshot.forEach(async (document) => {
+        const canvasID = document.id
+        const otherUserUID = document.data().uid
+        let user: galleryUser = { name: '', icon: '' }
+        await getDoc(doc(db, 'users', otherUserUID))
+          .then(async (userDocSnap) => {
+            if (userDocSnap.exists()) {
+              user = {
+                name: userDocSnap.data().name,
+                icon:
+                  userDocSnap.data().icon === ''
+                    ? Icon
+                    : userDocSnap.data().icon,
+              }
+            } else {
+              console.log('プロフィール情報が見つかりません')
+            }
+          })
+          .catch((error) => {
+            console.log('プロフィール情報の取得に失敗しました：', error)
+          })
+        console.log(document.data())
+        const canvas: shareCanvases = {
+          title: document.data().name,
+          id: canvasID,
+          uid: document.data().uid,
+          image: document.data().image,
+          updatedAt: document.data().updatedAt,
+          name: user.name,
+          avator: user.icon,
+          isLike:
+            likeHash.value[canvasID] !== undefined
+              ? likeHash.value[canvasID].isLike
+              : false,
+        }
+        galleries.value.set(canvasID, canvas)
+      })
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+}
+
+const likeColor = (isLike: boolean) => (isLike ? 'fill: red' : 'fill: #71767B')
+
+const clickLike = async (galleryId: string) => {
+  const gallery = galleries.value.get(galleryId)
+  if (gallery !== undefined) {
+    gallery.isLike = !gallery?.isLike
+    galleries.value.set(galleryId, gallery)
+    likeHash.value[galleryId] = {
+      isLike: gallery.isLike,
+      addedAt: Timestamp.now(),
+    }
+    await setDoc(doc(db, 'hearts', authStore.uid), likeHash.value)
+  }
+}
 </script>
 
 <template lang="pug">
 div(class="my-8 grid gap-4 xl:grid-cols-3 md:grid-cols-2")
-  div(v-for="(gallery, index) of galleries" :key="index" class="picture m-auto")
+  div(v-for="[key, gallery] in Array.from(galleries)" :key="key" class="picture m-auto")
     div(class="flex items-center relative")
-      label(for="modal" class="bg-gray-200 rounded-lg border border-gray-500" style="width: 320px; height: 180px" @click="resizeModal()")
-      svg(class="heart absolute cursor-pointer" viewBox="0 0 32 29.6" :style="likeColor(gallery.isLike)" style="right: 10px; bottom: 10px" @click="gallery.isLike = !gallery.isLike")
+      label(for="modal" style="width: 320px; height: 180px" @click="resizeModal(gallery.image)")
+        img(:src="gallery.image"  class="rounded-lg border border-gray-500")
+      svg(class="heart absolute cursor-pointer" viewBox="0 0 32 29.6" :style="likeColor(gallery.isLike)" style="right: 10px; bottom: 10px" @click="clickLike(gallery.id)")
         path(d="M23.6,0c-3.4,0-6.3,2.7-7.6,5.6C14.7,2.7,11.8,0,8.4,0C3.8,0,0,3.8,0,8.4c0,9.4,9.5,11.9,16,21.2c6.1-9.3,16-12.1,16-21.2C32,3.8,28.2,0,23.6,0z")
-    div(class="flex flex-col mt-1 ml-2")  
+    div(class="flex flex-col mt-1 ml-2")
       div(class="text-midnightBlue") {{ gallery.title }}
-      div(class="flex items-center mt-1") 
-        img(:src="gallery.avatar" class="tiny-avatar ring-2 bg-gray-200 ring-gray-700 ")
+      router-link(:to="{name: 'Users', params: { user_id: gallery.uid }}" class="flex items-center mt-1") 
+        img(:src="gallery.avator" class="tiny-avatar ring-2 bg-gray-200 ring-gray-700 ")
         div(class="text-midnightBlue text-sm pl-2") {{ gallery.name }}
 
 input(id="modal" type="checkbox" class="modal-toggle")
 label(for="modal" class="modal cursor-pointer")
-  div(class="bg-gray-200 rounded-lg border border-gray-500 relative" :style="modalStyle")
+  img(:src="testImage" class="rounded-lg border border-gray-500 relative" :style="modalStyle")
 
         
 </template>
